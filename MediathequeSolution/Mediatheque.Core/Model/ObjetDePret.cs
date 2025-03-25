e8d22b6a9bc9821d9233311284b660bd17697394
Rajirahoui <rajirahoui1@gmail.com>
2025-03-25 12:34:18 +0100

    finish

---
+++ MediathequeSolution/Mediatheque.Core/Model/ObjetDePret.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediatheque.Core.Model
+{
+
+    public class ObjetDePret
+    {
+        public string TitreDeLObjet { get; set; }
+        public string Emprunteur { get; set; }
+        public bool EstDisponible { get; private set; } = true;
+                
+        public ObjetDePret (string  titreDeLObjet , string emprunteur )
+        {
+            this.TitreDeLObjet = titreDeLObjet;
+            this.Emprunteur = emprunteur;
+            this.EstDisponible = true;
+
+        }
+
+        public bool Emprunte(string emprunteur)
+        {
+    if (!EstDisponible) return false;
+    
+    this.Emprunteur = emprunteur;
+    this.EstDisponible = false;
+    return true;
+}
+    }
+}
---
+++ MediathequeSolution/Mediatheque.CoreTests/ServiceTests/MediathequeServiceTests/GetNombreObjetShould.cs
@@ -0,0 +1,46 @@
+﻿using Mediatheque.Core.Model;
+using Mediatheque.Core.Service;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Mediatheque.CoreTests.ServiceTests.MediathequeServiceTests
+{
+    [TestClass]
+    public class GetNombreObjetShould
+    {
+        private MediathequeService _mediathequeService = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var notationServiceMock = new Mock<INotationService>().Object;
+            _mediathequeService = new MediathequeService(notationServiceMock);
+        }
+
+        [TestMethod]
+        public void Return0_WhenNoObject()
+        {
+            // Act
+            int actual = _mediathequeService.GetNombreObjet();
+
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void Return3_When3Objects()
+        {
+            // Arrange
+            _mediathequeService.AddObjet(new CD("Spice world", "Spice girl"));
+            _mediathequeService.AddObjet(new CD("Smash", "Offspring"));
+            _mediathequeService.AddObjet(new JeuxDeSociete("Jeux de 54 cartes", 3, 99, "Carrefour", TypeJeuxDeSociete.Carte));
+
+            // Act
+            int actual = _mediathequeService.GetNombreObjet();
+
+            // Assert
+            Assert.AreEqual(3, actual);
+        }
+    }
+}
